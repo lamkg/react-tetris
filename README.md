4856986836e632e6f859ea1e5747d14c0b148f17
lamkg <38263881+lamkg@users.noreply.github.com>
2023-07-10 16:23:19 -0700

    updating the styles component

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 <b><u><span style="font-size:larger;"> react tetris </span></u></b>
 
-
 **Project Overview:**
 
 Welcome to my Tetris Demo! This project holds a special place in my journey as a developer. I've always been fascinated by the captivating gameplay and timeless charm of the game.
@@ -9,13 +8,12 @@ Building the Tetris demo allowed me to challenge myself and expand my coding ski
 
 <b><u><span style="font-size:larger;"> Key Features: </span></u></b>
 
-Minimalism, the Marie Condo of tetris if you will. 
+Minimalism, the Marie Condo of tetris if you will.
 
 <b><u><span style="font-size:larger;"> Development Process </span></u></b>
 
 In development
 
-
 <b><u><span style="font-size:larger;"> Development: </span></u></b>
 
-React, CSS, Javascript
+React, CSS, Javascript, styled-components
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,21 +3,24 @@ import { createStage } from '../gameHelpers';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 
 const Tetris = () => {
-    return (
-        <div>
-        <Stage stage = {createStage()} />
+  return (
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
         <aside>
-            <div>
-            <Display text = 'Score' />
-            <Display text = 'Rows' />
-            <Display text = 'Level' />
-            </div>
-            <StartButton />
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
         </aside>
-    </div>
-    );
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
 };
 
 export default Tetris;
